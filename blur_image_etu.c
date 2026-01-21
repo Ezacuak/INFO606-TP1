987acf381e210c9d15895ed72def40a58e127fd5
Antonin <antonin.plard@etu.univ-savoie.fr>
2026-01-21 13:23:55 +0100

    Initial Commit

---
+++ blur_image_etu.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+typedef struct {
+        int width ;
+        int height ;
+        int max ;       //itensite max
+        char type[2] ;  //P5,P6
+        unsigned char *pixels ;  //pixmap
+} Image ;
+
+#define K_SIZE 5
+#define NB_PASS 10
+
+//noyau de convolution
+float kernel[K_SIZE][K_SIZE] = {
+    {1/256.0, 4/256.0, 6/256.0, 4/256.0, 1/256.0},
+    {4/256.0, 16/256.0, 24/256.0, 16/256.0, 4/256.0},
+    {6/256.0, 24/256.0, 36/256.0, 24/256.0, 6/256.0},
+    {4/256.0, 16/256.0, 24/256.0, 16/256.0, 4/256.0},
+    {1/256.0, 4/256.0, 6/256.0, 4/256.0, 1/256.0}
+};
+
+/* Lit un fichier sur un flux d'entrée */
+void read_pgm_image(FILE *file, Image* image) ;
+
+/* Ecrit un fichier sur un flux */
+void write_pgm_image(FILE *file, Image *image) ;
+
+/* Applique un flou gaussien sur l'image */
+void gaussian_blur(Image* src, Image* trg) ;
+
+
+void main(void){
+	FILE *f1, *f2 ; 
+	f1=fopen("rose.pgm", "r" );
+	f2=fopen("rose_blur.pgm", "w+" );
+
+	Image src, tmp ; 
+	
+	//1- init la source
+	read_pgm_image(f1, &src) ; 
+
+	//2- init les images temporaires  
+	tmp = src ; 
+	tmp.pixels=malloc(src.width*src.height);
+	
+	//3- blur de src -> tmp
+	gaussian_blur(&src, &tmp) ;
+
+	//4- ecrit l'image resultat apres NB_PASS passage 
+	write_pgm_image(f2, &tmp) ; 
+}
+
+/**
+* Fonction pour appliquer le flou gaussien
+*
+*/
+void gaussian_blur(Image* src, Image* trg) {
+	int K=K_SIZE/2 ;
+	float sum ; 
+	for (int iL = K; iL < src->height - K; iL++) {	//parcour des lignes 
+		for (int iC = K; iC < src->width - K; iC++) {//parcour des colonnes
+			sum = 0.0;
+     
+            		// Convolution avec le noiCau gaussien K_SIZEiLK_SIZE
+            		/*
+			for (int j = 0; j < K_SIZE ; j++) {
+                		for (int i = 0; i < K_SIZE; i++) {
+
+                
+				}
+            		}
+			*/
+            
+            		// Ecrire les pixels dans trg
+            		trg->pixels[trg->width*iL+iC] = src->pixels[trg->width*iL+iC];
+        }
+    }
+}
+
+/* Lit une image pgm sur le flux en paramtetre */
+void read_pgm_image(FILE *file, Image* image) {
+    fscanf(file, "%s\n%d %d\n%d\n", image->type, &image->width, &image->height, &image->max);
+    fgetc(file);
+    
+    int size=image->width * image->height ;
+
+    image->pixels = malloc(size);
+    fread(image->pixels, 1, size, file);
+   
+}
+
+/* Ecrit une image sur un fic */
+void write_pgm_image(FILE *file, Image *image) {
+    fprintf(file, "%s\n%d %d\n%d\n", image->type, image->width, image->height, image->max);
+    fwrite(image->pixels, 1, image->width * image->height, file);
+}
---
+++ bubble_sort.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * Trie à bulle simple.
+ *
+ * Verrification du temps d'execution en O(n²)
+ *
+ */
+
+// la structure data sera utilisée pour le passage de parametre
+// des threads
+typedef struct {
+  int *array;
+  int start;
+  int end;
+} Data;
+
+void *sort(void *arg);
+int *random_array(int N);
+
+int main(void) {
+
+  struct timespec start, end;
+  for (int i = 2; i <= (1 << 24); i *= 2) {
+
+    int *T = random_array(i);
+    Data d = {T, 0, i - 1};
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    // Code à mesurer ici
+    sort(&d);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    // Temps d'exécution
+    double elapsed =
+        (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+
+    // Affiche le temps d'éxécution sur la console
+    printf("%d\t%.9f\n", i, elapsed);
+  }
+
+  return 0;
+}
+
+int *random_array(int N) {
+  int *T = (int *)malloc(N * sizeof(int));
+  // nb de 0 a N-1
+  for (int i = 0; i < N; i++)
+    T[i] = i;
+
+  // suffle the T
+  srand(time(NULL));
+
+  // N interverti Nx
+  int i1, i2, tmp;
+  for (int i = 0; i < N; i++) {
+    i1 = rand() % N;
+    i2 = rand() % N;
+    tmp = T[i1];
+    T[i1] = T[i2];
+    T[i2] = tmp;
+  }
+
+  return T;
+}
+
+/**
+ * Algorithme de trie à bulle de complexité O(n²)
+ *
+ *
+ */
+void *sort(void *arg) {
+  // recuperation des struct array, left, right
+  Data *data = (Data *)arg;
+
+  int start = data->start;
+  int end = data->end;
+  int *array = data->array;
+  int tmp;
+
+  return NULL;
+}
